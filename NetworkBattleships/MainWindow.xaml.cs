109412b3f1319687a2fa1cccb0ea63815d50f35c
Nydus Born <90088743+NydusBorn@users.noreply.github.com>
2023-10-24 19:33:07 +0300

    add WinuiEX dependency

--- NetworkBattleships/MainWindow.xaml.cs
+++ NetworkBattleships/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using NetworkBattleships.Pages;
+using WinUIEx;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -22,7 +23,7 @@ namespace NetworkBattleships
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class MainWindow : WindowEx
     {
         public MainWindow()
         {
