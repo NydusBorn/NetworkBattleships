a939f37cce5b8e95a7c7ac95b70af5dfe926018b
Nydus Born <90088743+NydusBorn@users.noreply.github.com>
2023-10-31 18:07:35 +0300

    somewhat workable ship dragging

--- NetworkBattleships/Pages/GamePage.xaml.cs
+++ NetworkBattleships/Pages/GamePage.xaml.cs
@@ -1,15 +1,20 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage.Streams;
-using Windows.UI;
 using Windows.UI.Popups;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
 using WinRT;
+using Color = Windows.UI.Color;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -159,21 +164,23 @@ namespace NetworkBattleships.Pages
                 }
             };
             ShipsPanel.ItemsSource = ships;
-            Task.Run(() =>
-            {
-                DispatcherQueue.TryEnqueue(SetColors);
-            });
+            Task.Run(() => { DispatcherQueue.TryEnqueue(SetColors); });
         }
 
         private async void SetColors()
         {
-            await Task.Delay(500);
             var bt = PlayerGrid.Children[0] as Button;
+            while (bt.Background is null)
+            {
+                await Task.Delay(50);
+            }
+
             // Default dark theme is #0FFFFFFF
             CurrentDefaultButtonColor = (bt.Background as SolidColorBrush).Color;
             CurrentHoverButtonColor = new Color()
             {
-                R = (byte)(CurrentDefaultButtonColor.Value.R - 120), G = (byte)(CurrentDefaultButtonColor.Value.G - 120),
+                R = (byte)(CurrentDefaultButtonColor.Value.R - 120),
+                G = (byte)(CurrentDefaultButtonColor.Value.G - 120),
                 B = (byte)(CurrentDefaultButtonColor.Value.B - 120), A = CurrentDefaultButtonColor.Value.A
             };
             CurrentSunkButtonColor = new Color()
@@ -183,32 +190,137 @@ namespace NetworkBattleships.Pages
             };
             CurrentMissButtonColor = new Color()
             {
-                R = (byte)(Math.Clamp((int)CurrentDefaultButtonColor.Value.R, 100, 140)), G = (byte)(Math.Clamp((int)CurrentDefaultButtonColor.Value.G, 100, 140)),
+                R = (byte)(Math.Clamp((int)CurrentDefaultButtonColor.Value.R, 100, 140)),
+                G = (byte)(Math.Clamp((int)CurrentDefaultButtonColor.Value.G, 100, 140)),
                 B = (byte)(Math.Clamp((int)CurrentDefaultButtonColor.Value.B, 100, 140)), A = 120
             };
         }
 
         private void ShipDragOver(object sender, DragEventArgs e)
         {
-            e.AcceptedOperation = DataPackageOperation.Move;
-            // var blob = e.DataView.GetBitmapAsync().GetResults().OpenReadAsync().GetResults();
-            // var img = blob.As<BitmapImage>();
-            var img = new BitmapImage(new Uri("ms-appx:///Assets/Destroyer.svg"));
             var cursorPosition = e.GetPosition(PlayerGrid);
-            int row = (int)cursorPosition.Y / CellSize;
-            int col = (int)cursorPosition.X / CellSize;
-            for (int i = Math.Clamp(row, 1, FieldSide - 1);
-                 i <= Math.Clamp(row + (img.PixelHeight / CellSize), 1, FieldSide - 1);
-                 i++)
+            if (cursorPosition is { X: >= CellSize, Y: >= CellSize }) e.AcceptedOperation = DataPackageOperation.Move;
+            _currentCursorPosition = cursorPosition;
+        }
+
+        private CancellationTokenSource _dragCancellationTokenSource = new CancellationTokenSource();
+
+        private void ShipDragStart(object sender, DragStartingEventArgs e)
+        {
+            var images = ShipsPanel.ItemsSource as System.Collections.ObjectModel.ObservableCollection<Image>;
+            var cursor = e.GetPosition(ShipsPanel);
+            int selected = 0;
+            int currentHeight = 0;
+            foreach (var image in images)
             {
-                for (int j = Math.Clamp(col, 1, FieldSide - 1);
-                     j <= Math.Clamp(col + (img.PixelWidth / CellSize), 1, FieldSide - 1);
-                     j++)
+                currentHeight += (int)image.Height;
+                if (cursor.Y > currentHeight)
                 {
-                    var bt = PlayerGrid.Children[((i - 1) * (FieldSide - 1)) + j - 1] as Button;
-                    bt.Background = new SolidColorBrush(CurrentHoverButtonColor.Value);
+                    selected++;
+                }
+                else
+                {
+                    break;
                 }
             }
+            var img = images[selected];
+            _dragCancellationTokenSource = new CancellationTokenSource();
+            int width = (int)img.Width;
+            int height = (int)img.Height;
+            _rotation = Rotation.Down;
+            Task.Run(() => ShipDragProcess(height, width, _dragCancellationTokenSource.Token));
+        }
+
+        private Windows.Foundation.Point _currentCursorPosition;
+        private void PointerMove(object sender, PointerRoutedEventArgs e)
+        {
+            _currentCursorPosition = e.GetCurrentPoint(PlayerGrid).Position;
+            TextBlockStatus.Text = $"{_currentCursorPosition.X}\n{_currentCursorPosition.Y}";
+        }
+
+        enum Rotation
+        {
+            Up, Down, Left, Right
+        }
+
+        private Rotation _rotation;
+        
+        private async void ShipDragProcess(int imgHeight, int imgWidth, CancellationToken ct)
+        {
+            HashSet<Point> lastPositions = new HashSet<Point>();
+            while (true)
+            {
+                var currentCursorPosition = _currentCursorPosition;
+                HashSet<Point> newPositions = new HashSet<Point>();
+                int row = Math.Clamp((int)currentCursorPosition.Y / CellSize, 1, FieldSide - 1);
+                int col = Math.Clamp((int)currentCursorPosition.X / CellSize, 1, FieldSide - 1);
+                switch (_rotation)
+                {
+                    case Rotation.Up:
+                        row = Math.Clamp(row, 1 + (imgHeight / CellSize), FieldSide - 1);
+                        break;
+                    case Rotation.Down:
+                        row = Math.Clamp(row, 1, FieldSide - 1 - (imgHeight / CellSize));
+                        break;
+                    case Rotation.Left:
+                        col = Math.Clamp(col, 1 + (imgWidth / CellSize), FieldSide - 1);
+                        break;
+                    case Rotation.Right:
+                        col = Math.Clamp(col, 1, FieldSide - 1 - (imgWidth / CellSize));
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                if (_rotation is Rotation.Down or Rotation.Up)
+                {
+                    for (int i = row; i < row + (imgHeight / CellSize); i++)
+                    {
+                        
+                    }
+                }
+
+                foreach (var position in lastPositions.Where(x => !newPositions.Contains(x)))
+                {
+                    DispatcherQueue.TryEnqueue(() =>
+                        (PlayerGrid.Children[((position.X - 1) * (FieldSide - 1)) + position.Y - 1] as Button)
+                        .Background =
+                        new SolidColorBrush(CurrentDefaultButtonColor.Value));
+                }
+
+                foreach (var position in newPositions.Where(x=>!lastPositions.Contains(x)))
+                {
+                    DispatcherQueue.TryEnqueue(() =>
+                        (PlayerGrid.Children[((position.X - 1) * (FieldSide - 1)) + position.Y - 1] as Button)
+                        .Background =
+                        new SolidColorBrush(CurrentHoverButtonColor.Value));
+                }
+
+                lastPositions = newPositions;
+                await Task.Delay(10);
+                if (ct.IsCancellationRequested)
+                {
+                    break;
+                }
+            }
+            foreach (var position in lastPositions)
+            {
+                DispatcherQueue.TryEnqueue(() =>
+                    (PlayerGrid.Children[((position.X - 1) * (FieldSide - 1)) + position.Y - 1] as Button)
+                    .Background =
+                    new SolidColorBrush(CurrentDefaultButtonColor.Value));
+            }
+        }
+
+        private void ShipDrop(object o, DragEventArgs dragEventArgs)
+        {
+            _dragCancellationTokenSource.Cancel();
+            
+        }
+
+        private void ShipDropComplete(UIElement sender, DropCompletedEventArgs args)
+        {
+            _dragCancellationTokenSource.Cancel();
         }
     }
 }
